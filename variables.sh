71dea6a26e79bfe96f2bd7b15f5ce14d55b90fab
3Ln1Pr1 <49145284+3ln1pr1@users.noreply.github.com>
2019-05-28 15:38:57 +0200

    Add files via upload

---
+++ variables.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#titulo         :estado.sh
+#descripcion    :Este Script muestra un menú rapido para manejar opciones con las centralitas.
+#autor	        :@3ln1pr1
+#fecha          :2019-05-27
+#version        :0.4
+#uso		    :sh estado.sh
+#notas          :Install Nano and Emacs para usar el Script
+#bash_version   :4.1.5(1)-version
+#e-mail         :3ln1pr1&keemail.me
+#Empresa        :
+#Telefono       :@3ln1pr1
+#===3=====l======N=======1=========P=======r========1======@===3=====l======N=======1=========P=======r========1======@
+#============================================================================
+#           CSS PARA ENRRIQUECER TEXTO CON COLORES Y SALTOS DE LINEA        #
+#============================================================================
+# Bash css!
+Negro="\033[0;30m"
+Grisoscuro="\033[1;30m"
+Azul="\033[0;34m"
+Azulclaro="\033[1;34m"
+Verde="\033[0;32m"
+Verdeclaro="\033[1;32m"
+Cyan="\033[0;36m"
+Cyanclaro="\033[1;36m"
+Rojo="\033[0;31m"
+Rojoclaro="\033[1;31m"
+Purpura="\033[0;35m"
+Purpuraclaro="\033[1;35m"
+Marron="\033[0;33m"
+Amarillo="\033[1;33m"
+Grisclaro="\033[0;37m"
+Blanco="\033[1;37m"
+nc="\033[0m"
+
+# Ruta de los Ficheros de Actualizacion de los scripts.
+FOLDER1="scripts"
+ROOTSCRIPT=rootscripts.tar.gz
+URL=github.com/3ln1pr1/Scripts-en-Bash/blob/master/
+URL1=https://$URL$ROOTSCRIPT
+
+# A Partir de Aquí Variables para definir en todos los Scripts creados con creascript.sh
+VAR="VAR"
+VAR1="VAR1"
+VAR2="VAR2"
+
+
+
