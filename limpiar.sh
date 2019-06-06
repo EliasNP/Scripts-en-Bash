5ad7ab8e6fb947f89b74afecdee909497670e376
3lnipr1 <3lnipr1@keemail.me>
2019-06-06 10:27:31 +0200

    primer commit

--- limpiar.sh
+++ limpiar.sh
@@ -1,12 +1,12 @@
-    #!/bin/bash
+#!/bin/bash
 #===3=====l======N=======1=========P=======r========1======@===3=====l======N=======1=========P=======r========1======@
 #titulo         :Limpiar.sh
 #descripcion    :Este Script Limpia la Swap de Temporales para mejorar el rendimiento.
 #autor	        :3Ln1Pr1
 #fecha          :2019-06-06
 #version        :0.6
-#uso		    :./Limpiar.sh
-#notas          :Install Nano and Emacs para usar el Script
+#uso		:./Limpiar.sh
+#notas          :Install Nano and Emacs para editar el Script
 #bash_version   :4.1.5(1)-version
 #e-mail         :3ln1pr1(Arroba)keemail(dot)me
 #Empresa        :https://3ln1pr1.github.io/Bitacora/
