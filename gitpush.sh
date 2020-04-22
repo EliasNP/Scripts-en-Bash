087859bb8f24a2110579cace2cb61ddbc7c3c51f
enieva <correo@eliasnieva.com>
2020-04-22 16:47:10 +0200

    Probando autorellenado de bash scripts

--- gitpush.sh
+++ gitpush.sh
@@ -38,7 +38,6 @@ VARIABLE_EJEMPLO='valor de ejemplo'
 # Aqui iniciamos el Codigo.
 # Ejemplo de Mostrar resultados de Variables como Ejemplo.
 
-echo
 
 : 'COMENTARIO1 ESTE CODIGO ESTA COMENTADO PARA MOSTRAR LA SINTAXIS DE COMO DEBEN MOSTRARSE LAS VARIABLES.
 echo $FECHA
@@ -48,14 +47,62 @@ echo $IP
 echo $IP2
 echo $VARIABLE_EJEMPLO
 ' 
-                        
 
 : ' COMENTARIO2  ESTE CODIGO ESTA COMENTADO PARA MOSTRAR LA VARIABLES HEREDADAS DEL ARCHIVO /home/informatico/scripts/variables.sh
 
  #-----(↑)----(↑)------(↑)-------- HASTA AQUÍ LAS VARIABLES HEREDADAS ----(↑)------(↑)-----(↑)-------
-COMENTARIO2 ' 
+COMENTARIO2 '  
 
 echo "Cual va a ser el Texto del \"Commit\""
 read COMMIT
 
 git add . ; git commit -m "$COMMIT" ; git push
+
+function _declarada{
+    echo "Texto declarado dentro de la función _declarada"
+}
+# Usage: banner_color green "my title"
+
+function banner_color() {
+    local color=$1
+    shift
+
+    case $color in
+        black) color=0
+        ;;
+        red) color=1
+        ;;
+        green) color=2
+        ;;
+        yellow) color=3
+        ;;
+        blue) color=4
+        ;;
+        magenta) color=5
+        ;;
+        cyan) color=6
+        ;;
+        white) color=7
+        ;;
+        *) echo "color is not set"; exit 1
+        ;;
+    esac
+
+    local s=("$@") b w
+    for l in "${s[@]}"; do
+        ((w<${#l})) && { b="$l"; w="${#l}"; }
+    done
+    tput setaf $color
+    echo " =${b//?/=}=
+| ${b//?/ } |"
+    for l in "${s[@]}"; do
+        printf '| %s%*s%s |\n' "$(tput setaf $color)" "-$w" "$l" "$(tput setaf $color)"
+    done
+
+    echo "| ${b//?/ } |
+ =${b//?/=}="
+    tput sgr 0
+}
+
+banner_color green "tu madre"
+_declarada
