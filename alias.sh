1963d7102d4fa3e44252cce47c7b46ecf55a8edf
EliasNp <eliasnievap@protonmail.com>
2024-10-02 10:32:32 +0200

    subir nuevo fichero de aliases para .zshrc

---
+++ alias.sh
@@ -0,0 +1,40 @@
+# Alias Propios
+alias tmuxAll="tmux new-session \; split-window -h \; split-window -v \; attach"
+alias bastion='ssh bastion'
+alias tmuxClose="tmux kill-ses -t 0"
+
+# Alias de acceso a clientes.
+alias ssh-sala=" ssh -p 20022 -L 13128:127.0.0.1:3128 root@81.42.207.113"
+alias ssh-renove="ssh -p 20021 -L 13128:127.0.0.1:8888 root@195.55.244.173"
+alias ssh-tecno="ssh -p 20022 siptize@83.48.62.111 -L 13128:127.0.0.1:8888"
+alias ssh-mobliberica="ssh -p 20024 -L 13128:127.0.0.1:3128 root@109.167.105.159"
+alias ssh-ant='ssh -L 13128:127.0.0.1:3128 root@ant.cli.bio.es'
+alias ssh-demetrio='ssh -L 13128:127.0.0.1:3128 root@84.124.102.115'
+alias ssh-pikolinos='ssh -p 10022 -L 13128:127.0.0.1:3128 root@213.201.85.202'
+alias ssh-moraval='ssh -p 20022 -L 13128:127.0.0.1:3128 root@79.148.102.115'
+alias ssh-aqualai-murcia='ssh -p 20022 -L 13128:127.0.0.1:3128 root@80.24.99.250'
+alias ssh-aqualai-alicante='ssh -p 20022 -L 13128:127.0.0.1:3128 root@84.232.79.226'
+alias ssh-aquacar='ssh -p 10022 -L 13128:127.0.0.1:3128 root@80.35.68.242'
+alias ssh-comerplast='ssh -p 20022 -L 13128:127.0.0.1:3128 root@88.12.53.67'
+alias ssh-tecnicas-y-serv-esta='ssh -L 13128:127.0.0.1:3128 root@88.2.61.164'
+alias ssh-server='ssh -p 22 root@192.168.1.5'
+alias ssh-nordikas='ssh -L 13128:127.0.0.1:3128 root@80.32.233.43'
+alias ssh-gesfor='ssh -L 13128:127.0.0.1:3128 root@84.120.189.173'
+alias ssh-adetex='ssh -p 20022 -L 13128:127.0.0.1:3128 root@78.136.83.38'
+alias ssh-areto='ssh -p 20022 -L 13128:127.0.0.1:3128 root@areto.cli.bio.es'
+alias ssh-hiperber='ssh -p 20022 -L 13128:127.0.0.1:3128 root@80.59.244.250'
+alias ssh-materiales-bernal='ssh -p 20022 -L 13128:127.0.0.1:3128 root@bernal.cli.bio.es'
+alias ssh-coquillat='ssh -p 20022 -L 13128:127.0.0.1:3128 root@84.127.237.26'
+alias ssh-intesa='ssh -p 20022 -L 13128:127.0.0.1:3128 root@90.175.196.161'
+alias ssh-teleges1='ssh -p 20022 -L 13128:127.0.0.1:3128 root@84.127.243.60'
+alias ssh-triunf='ssh -p 20022 -L 13128:127.0.0.1:3128 root@89.29.192.22'
+alias ssh-teleges1-server='ssh -p 20023 -L 13128:127.0.0.1:3128 root@84.127.243.60'
+alias ssh-mobliberica='ssh -p 20022 -L 13128:127.0.0.1:3128 root@213.0.2.121'
+alias ssh-sisnet="ssh siptize@185.154.253.17"
+
+## Clientes de CableWorld - OnPremisse
+alias ssh-ontinyent="ssh jpmsiptize@185.59.51.85"
+alias ssh-novelda="ssh siptize@89.29.160.37"
+alias ssh-elda="ssh siptize@89.29.188.27"
+alias ssh-elda="ssh siptize@176.100.213.137"
+# - - - -
